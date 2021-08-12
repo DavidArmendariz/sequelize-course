0df58af78a6f3a3d4b49ebc7c870f3510b6522d5
David Armendariz <darmendariz1998@outlook.com>
2021-08-12 00:06:02 -0500

    Added refresh token migration

---
+++ src/database/migrations/20210812050134-create_refresh_token_table.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('RefreshTokens', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      token: {
+        type: Sequelize.TEXT,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      UserId: {
+        type: Sequelize.INTEGER,
+        references: { model: { tableName: 'Users' }, key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('RefreshTokens');
+  },
+};
--- src/models/refresh-token.js
+++ src/models/refresh-token.js
@@ -3,12 +3,15 @@ import { Model, DataTypes } from 'sequelize';
 export default (sequelize) => {
   class RefreshToken extends Model {
     static associate(models) {
-      RefreshToken.belongsTo(models.User);
+      RefreshToken.belongsTo(models.User, {
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      });
     }
   }
 
   RefreshToken.init(
-    { token: { type: DataTypes.STRING } },
+    { token: { type: DataTypes.TEXT } },
     { sequelize, modelName: 'RefreshToken' }
   );
 
