df9ff06a309e86c74efd42b7b3bf8268a1f19bab
David Armendariz <darmendariz1998@outlook.com>
2021-08-11 23:19:12 -0500

    Added users table migration

---
+++ src/database/migrations/20210812040850-create_users_table.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      email: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      username: {
+        type: Sequelize.STRING(50),
+        unique: true,
+      },
+      firstName: {
+        type: Sequelize.STRING(50),
+      },
+      lastName: {
+        type: Sequelize.STRING(50),
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  },
+};
