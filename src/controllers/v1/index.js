ebed58d09c8ba1686031e165148a67c6242bce8f
David Armendariz <darmendariz1998@outlook.com>
2021-08-14 18:34:00 -0500

    Token controller tests part 1

--- src/controllers/v1/index.js
+++ src/controllers/v1/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import registerRouter from './register';
 import loginRouter from './login';
+import tokenRouter from './token';
 
 const router = Router();
 
 router.use(registerRouter);
 router.use(loginRouter);
+router.use(tokenRouter);
 
 export default router;
---
+++ tests/controllers/v1/token.test.js
@@ -0,0 +1,72 @@
+import TestsHelpers from '../../tests-helpers';
+import models from '../../../src/models';
+import request from 'supertest';
+import JWTUtils from '../../../src/utils/jwt-utils';
+
+describe('register', () => {
+  let app;
+  let newUserResponse;
+
+  beforeAll(async () => {
+    await TestsHelpers.startDb();
+    app = TestsHelpers.getApp();
+  });
+
+  afterAll(async () => {
+    await TestsHelpers.stopDb();
+  });
+
+  beforeEach(async () => {
+    await TestsHelpers.syncDb();
+    newUserResponse = await TestsHelpers.registerNewUser({
+      email: 'test@example.com',
+      password: 'Test123#',
+    });
+  });
+
+  describe('requiresAuth middleware', () => {
+    it('should fail if the refresh token is invalid', async () => {
+      const response = await request(app)
+        .post('/v1/token')
+        .set('Authorization', 'Bearer invalidtoken')
+        .send()
+        .expect(401);
+      expect(response.body.success).toEqual(false);
+      expect(response.body.message).toEqual('Invalid token');
+    });
+
+    it('should fail if no authorization header is present', async () => {
+      const response = await request(app).post('/v1/token').send().expect(401);
+      expect(response.body.success).toEqual(false);
+      expect(response.body.message).toEqual('Authorization header not found');
+    });
+
+    it('should fail if the authorization header is malformed', async () => {
+      const response = await request(app)
+        .post('/v1/token')
+        .set('Authorization', 'Invalid')
+        .send()
+        .expect(401);
+      expect(response.body.success).toEqual(false);
+      expect(response.body.message).toEqual('Bearer token malformed');
+    });
+  });
+
+  describe('errors middleware', () => {
+    it('should return 500 if something went wrong', async () => {
+      const jwtUtilsSpy = jest.spyOn(JWTUtils, 'generateAccessToken');
+      jwtUtilsSpy.mockImplementation(() => {
+        throw Error('test error');
+      });
+      const refreshToken = newUserResponse.body.data.refreshToken;
+      const response = await request(app)
+        .post('/v1/token')
+        .set('Authorization', `Bearer ${refreshToken}`)
+        .send()
+        .expect(500);
+      jwtUtilsSpy.mockRestore();
+      expect(response.body.success).toEqual(false);
+      expect(response.body.message).toEqual('test error');
+    });
+  });
+});
