e58f6c677aed673e0b40df142ee07b34faece649
David Armendariz <darmendariz1998@outlook.com>
2021-08-14 12:13:30 -0500

    Adding register controller

---
+++ src/controllers/v1/register.js
@@ -0,0 +1,35 @@
+import { Router } from 'express';
+import models from '../../models';
+import asyncWrapper from '../../utils/asyncWrapper';
+import JWTUtils from '../../utils/jwt-utils';
+
+const router = Router();
+const { User } = models;
+
+router.post(
+  '/register',
+  asyncWrapper(async (req, res) => {
+    const { email } = req.body;
+    const user = await User.findOne({ where: { email } });
+
+    if (user) {
+      return res
+        .status(200)
+        .send({ success: false, message: 'User already exists' });
+    }
+
+    const payload = { email };
+    const accessToken = JWTUtils.generateAccessToken(payload);
+    const refreshToken = JWTUtils.generateRefreshToken(payload);
+    await User.createNewUser({ ...req.body, refreshToken });
+
+    return res.status(200).send({
+      success: true,
+      message: 'User successfully registered',
+      data: {
+        accessToken,
+        refreshToken,
+      },
+    });
+  })
+);
