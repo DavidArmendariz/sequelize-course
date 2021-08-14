e8e1c337a8d66baa4baceac66b839e3f8df676e0
David Armendariz <darmendariz1998@outlook.com>
2021-08-14 15:15:24 -0500

    Added token controller

---
+++ src/controllers/v1/token.js
@@ -0,0 +1,38 @@
+import { Router } from 'express';
+import models from '../../models';
+import asyncWrapper from '../../utils/asyncWrapper';
+import JWTUtils from '../../utils/jwt-utils';
+import requiresAuth from '../../middlewares/requiresAuth';
+
+const router = Router();
+const { User, RefreshToken } = models;
+
+router.post(
+  '/token',
+  requiresAuth('refreshToken'),
+  asyncWrapper(async (req, res) => {
+    const {
+      jwt: { email },
+    } = req.body;
+    const user = await User.findOne({
+      where: { email },
+      include: RefreshToken,
+    });
+    const savedToken = user.RefreshToken;
+
+    if (!savedToken || !savedToken.token) {
+      return res
+        .status(401)
+        .savedToken({ success: false, message: 'You must log in first' });
+    }
+
+    const payload = { email };
+    const newAccessToken = JWTUtils.generateAccessToken(payload);
+
+    return res
+      .status(200)
+      .send({ success: true, data: { accessToken: newAccessToken } });
+  })
+);
+
+export default router;
