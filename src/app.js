702db1be64177bb1f501796398276740c974c18f
David Armendariz <darmendariz1998@outlook.com>
2021-08-14 12:05:50 -0500

    Structuring the controllers

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import environment from './config/environment';
 import logger from 'morgan';
 import errorsMiddleware from './middlewares/errors';
+import { v1Routes } from './controllers';
 
 export default class App {
   constructor() {
@@ -15,6 +16,7 @@ export default class App {
   }
 
   setRoutes() {
+    this.app.use('/v1', v1Routes);
     this.app.use(errorsMiddleware);
   }
 
---
+++ src/controllers/index.js
@@ -0,0 +1,3 @@
+import v1Routes from './v1';
+
+export { v1Routes };
